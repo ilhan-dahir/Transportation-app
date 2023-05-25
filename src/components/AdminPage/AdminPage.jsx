648df45bbda07254c37077cb31a9ccd4fbe9fcdc
ilhan-dahir <ahayad91@gmail.com>
2023-05-25 17:37:39 -0500

    Rendered Rides table to admins view

--- src/components/AdminPage/AdminPage.jsx
+++ src/components/AdminPage/AdminPage.jsx
@@ -1,8 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import useReduxStore from '../../hooks/useReduxStore';
+
 
 function AdminPage() {
-    const store = useSelector((store) => store);
+    const store = useReduxStore();
+
+    console.log('store>>>>', store)
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch({ type: 'SAGA_FETCH_RIDES' });
@@ -11,6 +15,32 @@ function AdminPage() {
     return (
         <div>
             <h2>Rides to Assign!</h2>
+            <table className="simpleTable">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Pickup</th>
+                        <th>Dropoff</th>
+                        <th>Pickup Time</th>
+                        <th>Dropoff Time</th>
+                        <th>Completed</th>
+                        <th>Assigned Driver</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {store.rides.map((ride, index) => (
+                        <tr key={index}>
+                            <td>{index + 1}</td>
+                            <td>{ride.pickup_location}</td>
+                            <td>{ride.dropoff_location}</td>
+                            <td>{ride.pickup_time}</td>
+                            <td>{ride.dropoff_time}</td>
+                            <td>{ride.is_Complete ? `YES` : `NO`}</td>
+                            <td>{ride.user_id === null ? `NOT ASSIGNED` : ride.user_id}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     );
 
--- src/redux/sagas/rides.saga.js
+++ src/redux/sagas/rides.saga.js
@@ -4,7 +4,6 @@ import { put, takeLatest } from 'redux-saga/effects';
 function* fetchRides() {
     try {
         const response = yield axios.get('/api/rides');
-        console.log('rides tables please', response.data);
         yield put({ type: 'SET_RIDES', payload: response.data });
     } catch (err) {
         console.log('error in fetchRides Saga', err);
